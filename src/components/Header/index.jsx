481f005a5ab111bd17db93ca875c067677c8f826
Cesar Martinez <cecam92@hotmail.com>
2021-04-13 09:17:17 +1000

    gastby build trick

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -2,25 +2,30 @@ import React from "react";
 import { useSpring, animated as a } from "react-spring";
 
 import { Title, SubTitle, Container } from "./styles";
+
+const isBrowser = typeof window !== "undefined";
+
 const Header = ({ data }) => {
-  const headerProps = useSpring({
-    from: {
-      transform: "scale(1)",
-      opacity: 1,
-    },
-    opacity: 1 - window.scrollY / 100,
-    transform: `scale(${data})`,
-  });
-  return (
-    <header>
-      <a.div style={headerProps}>
-        <Container>
-          <Title>Cesar Martinez</Title>
-          <SubTitle>Web & Mobile Developer</SubTitle>
-        </Container>
-      </a.div>
-    </header>
-  );
+  if (isBrowser) {
+    const headerProps = useSpring({
+      from: {
+        transform: "scale(1)",
+        opacity: 1,
+      },
+      opacity: 1 - window.scrollY / 100,
+      transform: `scale(${data})`,
+    });
+    return (
+      <header>
+        <a.div style={headerProps}>
+          <Container>
+            <Title>Cesar Martinez</Title>
+            <SubTitle>Web & Mobile Developer</SubTitle>
+          </Container>
+        </a.div>
+      </header>
+    );
+  }
 };
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -10,61 +10,66 @@ import { useSpring, animated as a } from "react-spring";
 
 import { Container } from "../styles";
 
+const isBrowser = typeof window !== "undefined";
+
 export default function Home() {
   const [data, setData] = useState(1);
   const [stick, setStick] = useState(false);
-
-  const fadeProps = useSpring({
-    position: "absolute",
-    top: "20vh",
-    paddingTop: 10,
-    opacity: stick ? 1 : 0,
-    backgroundColor: stick ? "#121212" : "#121212",
-  });
-  function cutNumber(num) {
-    const MIN = 1;
-    const MAX = 96;
-    const parsed = parseInt(num);
-    return Math.min(Math.max(parsed, MIN), MAX);
-  }
-  function scrolling() {
-    setData(cutNumber(window.scrollY));
-  }
-  function getStick() {
-    if (data >= 96) {
-      setStick(true);
-    } else {
-      setStick(false);
+  if (isBrowser) {
+    const fadeProps = useSpring({
+      position: "absolute",
+      top: "20vh",
+      paddingTop: 10,
+      opacity: stick ? 1 : 0,
+      backgroundColor: stick ? "#121212" : "#121212",
+    });
+    function cutNumber(num) {
+      const MIN = 1;
+      const MAX = 96;
+      const parsed = parseInt(num);
+      return Math.min(Math.max(parsed, MIN), MAX);
     }
+    function scrolling() {
+      setData(cutNumber(window.scrollY));
+    }
+    function getStick() {
+      if (data >= 96) {
+        setStick(true);
+      } else {
+        setStick(false);
+      }
+    }
+    useEffect(() => {
+      window.addEventListener("scroll", scrolling);
+      getStick();
+
+      return () => {
+        window.removeEventListener("scroll", scrolling);
+      };
+      // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data]);
+
+    return (
+      <>
+        <Header data={data} />
+        <a.div style={fadeProps}>
+          {stick && (
+            <>
+              <Nav />
+            </>
+          )}
+          <Container>
+            {/* <Nav /> */}
+            <About />
+            <Separator />
+            <Portfolio />
+            <Technologies />
+            <Footer />
+          </Container>
+        </a.div>
+      </>
+    );
+  } else {
+    return <></>;
   }
-  useEffect(() => {
-    window.addEventListener("scroll", scrolling);
-    getStick();
-
-    return () => {
-      window.removeEventListener("scroll", scrolling);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [data]);
-
-  return (
-    <>
-      <Header data={data} />
-      <a.div style={fadeProps}>
-        {stick && (
-          <>
-            <Nav />
-          </>
-        )}
-        <Container>
-          {/* <Nav /> */}
-          <About />
-          <Separator />
-          <Portfolio />
-          <Technologies />
-          <Footer />
-        </Container>
-      </a.div>
-    </>
-  );
 }
